c82a9b4d1e06a637a08fe6ac44148565b1257e59
Akshay Rathod <139254824+Akshay8087@users.noreply.github.com>
2025-04-29 13:29:19 +0530

    Update and rename build.sh to .j

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Install system dependencies
-apt-get update && apt-get install -y build-essential python3-dev
-
-# Upgrade pip
-pip install --upgrade pip
-
-# Install Python packages
-pip install -r requirements.txt
